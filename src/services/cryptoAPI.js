09336fd2a19f90f6aea24333d2db6c5aa0b77780
rsayyed591 <rehansayyed591@gmail.com>
2024-08-24 02:08:17 +0530

    created an api

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const cryptoAPIHeaders ={
-      'x-rapidapi-key': '570b86dafdmsh0d1e1c3b0b6d501p12db1bjsnd77068d5011b',
-      'x-rapidapi-host': 'coinranking1.p.rapidapi.com'    
+    //   'x-rapidapi-key': '570b86dafdmsh0d1e1c3b0b6d501p12db1bjsnd77068d5011b',
+    //   'x-rapidapi-host': 'coinranking1.p.rapidapi.com'    
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/';
