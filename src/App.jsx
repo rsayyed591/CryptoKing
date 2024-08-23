17e065c15b0d604548a52f005af73bd36b0021a1
rsayyed591 <rehansayyed591@gmail.com>
2024-08-24 01:35:53 +0530

    Added till navbar

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Routes, Route, Link } from 'react-router-dom';
+import {Layout, Typography, Space } from 'antd';
+import {Navbar,Exchanges,Homepage,Cryptocurrencies,News,CryptoDetails} from './components';
+import './App.css';
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+          <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  );
+}
+
+export default App
